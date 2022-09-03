b2ce5d837cc9600e1ad7074a4ec134d9922109ec
luowenlin <809714326@qq.com>
2022-09-03 21:19:34 +0800

    WebView的用法-添加注释

--- WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
+++ WebViewTest/app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = (WebView) findViewById(R.id.web_view);
+        // 使WebView支持js
         webView.getSettings().setJavaScriptEnabled(true);
+        // 当需要从一个网页跳转到另外一个网页的时候，我们希望目标网页仍然在当前WebView中显示，而不是去打开系统浏览器
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.future-science.cn");
     }
