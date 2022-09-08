d8039b449e1868b0a6d5fd5e328dba800420659e
luowenlin <809714326@qq.com>
2022-09-08 23:01:37 +0800

    解析XML-Pull解析-SAX解析 解析JSON-JSONObject-GSON

--- NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xmlpull.v1.XmlPullParser;
@@ -17,8 +22,10 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 import javax.xml.parsers.SAXParserFactory;
 
@@ -32,26 +39,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     /**
-        使用HttpUrlConnection
-        首先new一个URL对象，并传入网络地址    URL url = new URL("https://www.baidu.com")
-                        ↓
-        然后调用openConnection方法即可      HttpURLConnection connection = (HttpURLConnection) url.openConnection
-                        ↓
-        在获取到HttpUrlConnection的实例之后，我们可以设置一下HTTP请求所使用的方法(GET / POST) connection.setRequestMethod("GET")
-                        ↓
-        调用getInputStream()方法就可以获取到服务器返回的输入流了 InputStream in = connection.getInputStream();
-                        ↓
-        调用disconnect()方法将这个HTTP连接关闭掉 connection.disconnect()
-
-        设置连接超时：connection.setConnectTimeout(8000)
-        设置读取超时：connection.setReadTimeout(8000)
-
-        如果我们想要提交数据给服务器，只需要将HTTP请求的方法改成POST
-        connection.setRequestMethod("POST");
-        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-        out.writeBytes("username=admin&password=123456");
+     * 使用HttpUrlConnection
+     * 首先new一个URL对象，并传入网络地址    URL url = new URL("https://www.baidu.com")
+     * ↓
+     * 然后调用openConnection方法即可      HttpURLConnection connection = (HttpURLConnection) url.openConnection
+     * ↓
+     * 在获取到HttpUrlConnection的实例之后，我们可以设置一下HTTP请求所使用的方法(GET / POST) connection.setRequestMethod("GET")
+     * ↓
+     * 调用getInputStream()方法就可以获取到服务器返回的输入流了 InputStream in = connection.getInputStream();
+     * ↓
+     * 调用disconnect()方法将这个HTTP连接关闭掉 connection.disconnect()
+     * <p>
+     * 设置连接超时：connection.setConnectTimeout(8000)
+     * 设置读取超时：connection.setReadTimeout(8000)
+     * <p>
+     * 如果我们想要提交数据给服务器，只需要将HTTP请求的方法改成POST
+     * connection.setRequestMethod("POST");
+     * DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+     * out.writeBytes("username=admin&password=123456");
      */
-    TextView  responseText;
+    TextView responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,37 +77,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /**  使用OkHttp
-        在使用OkHttp之前，需要在项目中添加OkHttp库的依赖
-            implementation('com.squareup.okhttp3:okhttp:3.4.1')
-        首先创建一个OkHttpClient的实例
-        OkHttpClient client = new okHttpClient();
-                            ↓
-        接下来如果想要发起一条http请求，就需要创建一个request对象
-        Request request = new Request.Builder().build();
-        这只是创建了一个空的request对象，可以在build方法前连缀很多其他方法来丰富这个request对象，比如：
-        Request request = new Request.Builder()
-                    .url("https://www.baidu.com")
-                    .build();
-                                    ↓
-        之后调用OkHttpClient的newCall方法来创建一个Call对象，并调用他的execute()方法来发送请求并获取服务器返回的数据
-        Response response = client.newCall(request).execute();
-                                    ↓
-        其中response对象就是服务器返回的数据了，我们可以使用如下写法来获取返回的具体内容
-        String responseData = response.body().string();
-
-        如果是发起一条post请求就需要先构建出一个requestBody对象来存放待提交的参数
-        RequestBody requestBody = new FormBody.Builder()
-                    .add("username", "admin")
-                    .add("password", "123456")
-                    .build();
-        然后在Request.Builder中调用一下post方法，并将requestBody对象传入
-        Request request = new Request.Builder()
-                .url("https://www.baidu.com")
-                .post(requestBody)
-                .build();
-        接下来的操作就和get请求一样了，调用execute方法来发送请求并获取服务器返回的数据即可
-
+    /**
+     * 使用OkHttp
+     * 在使用OkHttp之前，需要在项目中添加OkHttp库的依赖
+     * implementation('com.squareup.okhttp3:okhttp:3.4.1')
+     * 首先创建一个OkHttpClient的实例
+     * OkHttpClient client = new okHttpClient();
+     * ↓
+     * 接下来如果想要发起一条http请求，就需要创建一个request对象
+     * Request request = new Request.Builder().build();
+     * 这只是创建了一个空的request对象，可以在build方法前连缀很多其他方法来丰富这个request对象，比如：
+     * Request request = new Request.Builder()
+     * .url("https://www.baidu.com")
+     * .build();
+     * ↓
+     * 之后调用OkHttpClient的newCall方法来创建一个Call对象，并调用他的execute()方法来发送请求并获取服务器返回的数据
+     * Response response = client.newCall(request).execute();
+     * ↓
+     * 其中response对象就是服务器返回的数据了，我们可以使用如下写法来获取返回的具体内容
+     * String responseData = response.body().string();
+     * <p>
+     * 如果是发起一条post请求就需要先构建出一个requestBody对象来存放待提交的参数
+     * RequestBody requestBody = new FormBody.Builder()
+     * .add("username", "admin")
+     * .add("password", "123456")
+     * .build();
+     * 然后在Request.Builder中调用一下post方法，并将requestBody对象传入
+     * Request request = new Request.Builder()
+     * .url("https://www.baidu.com")
+     * .post(requestBody)
+     * .build();
+     * 接下来的操作就和get请求一样了，调用execute方法来发送请求并获取服务器返回的数据即可
      */
     private void sendRequestWithOkHttp() {
         new Thread(new Runnable() {
@@ -117,13 +124,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Request request = new Request.Builder()
 //                            .url("https://www.baidu.com")
                             // 指定访问的服务器地址是电脑本机
-                            .url("http://10.0.2.2/get_data.xml")
+//                            .url("http://10.0.2.2/get_data.xml")
+                            .url("http://10.0.2.2/get_data.json")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
 //                    showResponse(responseData);
 //                    parseXMLWithPull(responseData);
-                    parseXmlWithSAX(responseData);
+//                    parseXmlWithSAX(responseData);
+//                    parseJSONWithJSONObject(responseData);
+                    parseJSONWithGSON(responseData);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -131,6 +141,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }).start();
     }
 
+    /**
+     * 使用GSON解析JSON文件
+     */
+    private void parseJSONWithGSON(String responseData) {
+        Gson gson = new Gson();
+        // 因为存在泛型擦除，就无法进行反序列化，因此使用TypeToken来获取类型
+        Type type = new TypeToken<List<App>>(){}.getType();
+        List<App> appList = gson.fromJson(responseData, type);
+        for (App app : appList) {
+            Log.d("MainActivity", "id is " + app.getId());
+            Log.d("MainActivity", "name is " + app.getName());
+            Log.d("MainActivity", "version is " + app.getVersion());
+        }
+    }
+
+    /**
+     * 解析JSON文件
+     */
+    private void parseJSONWithJSONObject(String responseData) {
+        try {
+            JSONArray jsonArray = new JSONArray(responseData);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                String id = jsonObject.getString("id");
+                String name = jsonObject.getString("name");
+                String version = jsonObject.getString("version");
+                Log.d("MainActivity", "id is " + id);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "version is " + version);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * SAX方式解析XML文件
+     */
     private void parseXmlWithSAX(String responseData) {
         try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
@@ -145,6 +195,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
+    /**
+     * PULL方式解析XML文件
+     */
     private void parseXMLWithPull(String responseData) {
         try {
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -172,8 +226,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         }
                         break;
                     }
-                        // 完成解析某个节点
-                        //  每当解析完成一个app节点后就将获取到的内容打印出来
+                    // 完成解析某个节点
+                    //  每当解析完成一个app节点后就将获取到的内容打印出来
                     case XmlPullParser.END_TAG: {
                         if ("app".equals(nodeName)) {
                             Log.d("MainActivity", "id is " + id);
@@ -187,7 +241,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 eventType = xmlPullParser.next();
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -222,7 +276,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (reader != null) {
                         try {
                             reader.close();
-                        }catch (Exception e) {
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
